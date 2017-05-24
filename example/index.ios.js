0dc42da3434593afdf5e0e5213eb49854c922f19
c-bata <contact@c-bata.link>
2017-05-25 02:18:09 +0900

    Implement FocusedScrollView

--- example/index.ios.js
+++ example/index.ios.js
@@ -11,35 +11,119 @@ import {
   Text,
   View,
   Dimensions,
+  ScrollView,
 } from 'react-native';
 
+class TestItemComponent extends Component {
+  render() {
+    let message = (<Text>Not Focused!!</Text>);
+    let style = {};
+    if (this.props.isFocused) {
+      message = (<Text>Focused!!</Text>);
+      style = {backgroundColor: "#bbb"}
+    }
+    return (
+      <View style={[styles.itemComponent, style]} onLayout={this.props.onLayout}>
+        <Text>Hello World.</Text>
+        <Text>I am Masashi Shibata.</Text>
+        {message}
+      </View>
+    )
+  }
+}
+
+class FocusedScrollView extends Component {
+  constructor(props) {
+    super(props);
+
+    this.getScrollViewSize = this.getScrollViewSize.bind(this);
+    this.getCenterY = this.getCenterY.bind(this);
+    this.handleScroll = this.handleScroll.bind(this);
+
+    this.state = {
+      scrollViewX: 0,
+      scrollViewY: 0,
+      scrollViewHeight: 0,
+      scrollViewWidth: 0,
+      focusedItem: null,
+    }
+  }
+
+  getCenterY() {
+    return (this.state.offsetY - this.state.scrollViewY) + (this.state.scrollViewHeight / 2)
+  }
+
+  getScrollViewSize(event) {
+    const {x, y, width, height} = event.nativeEvent.layout;
+    this.setState({
+      scrollViewX: x,
+      scrollViewY: y,
+      scrollViewWidth: width,
+      scrollViewHeight: height,
+    })
+  }
+
+  getDisplaySize(index) {
+    return (event) => {
+      const { x, y, width, height } = event.nativeEvent.layout;
+      let payload = {}
+      payload["item" + index] = {index, x, y, width, height};
+      this.setState(payload);
+      return index, x, y, width, height
+    }
+  }
+
+  handleScroll(event) {
+    this.setState({
+      offsetY: event.nativeEvent.contentOffset.y,
+      offsetX: event.nativeEvent.contentOffset.x,
+    });
+  }
+
+  render() {
+    console.log(this.getCenterY());
+    const focusedKey = this.state.focusedItem
+    const children = this.props.children.map((item, i) => {
+      const size = this.state["item" + i]
+      let isFocused;
+      if (typeof size === "undefined") {
+        isFocused = false
+      } else {
+        console.log("FOCUS?????????\n" + this.getCenterY())
+        isFocused = Math.abs(size.y - this.getCenterY()) < 100;
+      }
+      return React.cloneElement(item, {
+        onLayout: this.getDisplaySize(i),
+        isFocused: isFocused,
+      })
+    });
+
+    return (
+      <ScrollView {...this.props} onScroll={this.handleScroll} scrollEventThrottle={100} onLayout={this.getScrollViewSize}>
+        {children}
+     </ScrollView>
+    )
+  }
+}
 
 export default class example extends Component {
   constructor(props) {
     super(props);
-    this.displaySize = this.displaySize.bind(this);
   }
   
-  displaySize(event) {
-    var {x, y, width, height} = event.nativeEvent.layout;
-    console.log("x: " + x + ", y: " + y);
-    console.log("width: " + width + ", height: " + height);
-  }
-
   render() {
     const dim = Dimensions.get('screen');
-    console.log("windowWidth: " + dim.width + ", windowHeight: " + dim.height);
     return (
-      <View style={styles.container} onLayout={this.displaySize}>
-        <Text style={styles.welcome}>
-          Welcome to React Native!
-        </Text>
-        <Text style={styles.instructions}>
-          To get started, edit index.ios.js
-        </Text>
-        <Text style={styles.instructions} onLayout={this.displaySize}>
-          Press Cmd+R to reload,{'\n'}
-        </Text>
+      <View style={styles.container}>
+        <FocusedScrollView style={styles.scroll}>
+          <TestItemComponent key={1} />
+          <TestItemComponent key={2} />
+          <TestItemComponent key={3} />
+          <TestItemComponent key={4} />
+          <TestItemComponent key={5} />
+          <TestItemComponent key={6} />
+          <TestItemComponent key={7} />
+        </FocusedScrollView>
       </View>
     );
   }
@@ -51,17 +135,17 @@ const styles = StyleSheet.create({
     justifyContent: 'center',
     alignItems: 'center',
     backgroundColor: '#F5FCFF',
+    top: 21,
   },
-  welcome: {
-    fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
+  scroll: {
+    width: "100%",
   },
-  instructions: {
-    textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5,
-  },
+  itemComponent: {
+    borderWidth: 1,
+    borderColor: "#333",
+    height: 200,
+    backgroundColor: "#ddd",
+  }
 });
 
 AppRegistry.registerComponent('example', () => example);
